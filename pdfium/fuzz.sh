095fc9f1451020007efadc0841bacc5f6bc924dc
root <sontapaa.jokulainen@gmail.com>
2025-10-29 21:27:47 +0200

    Blah

--- pdfium/fuzz.sh
+++ pdfium/fuzz.sh
@@ -5,6 +5,6 @@ cp /home/oof/new_pdf_mutator/pdfium/*.py .
 cp /home/oof/new_pdf_mutator/resources.pkl .
 export ASAN_OPTIONS=alloc_dealloc_mismatch=0:allocator_may_return_null=1:halt_on_error=0
 # pdf_corpus
-ASAN_OPTIONS=alloc_dealloc_mismatch=0:allocator_may_return_null=1:halt_on_error=1:abort_on_error=1 LIBFUZZER_PYTHON_MODULE=daemon PYTHONPATH=. ./pdfium_fuzzer -dict=pdfium_fuzzer.dict -timeout=3 ./samplecorpus/
+ASAN_OPTIONS=alloc_dealloc_mismatch=0:allocator_may_return_null=1:halt_on_error=1:abort_on_error=1 LIBFUZZER_PYTHON_MODULE=daemon PYTHONPATH=. ./pdfium_fuzzer -fork=1 -ignore_crashes=1 -dict=pdfium_fuzzer.dict -timeout=10 -rss_limit_mb=0 ./pdf_corpus/
 
 
