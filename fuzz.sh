5c172197f55aa2cfe0e5e1929f246812a14bb1fc
root <sontapaa.jokulainen@gmail.com>
2025-10-31 02:38:55 +0200

    Blah

--- fuzz.sh
+++ fuzz.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-cp /home/oof/new_pdf_mutator/mutator.py .
+cp /home/oof/new_pdf_mutator/pdfium/newmutator.py ./mutator.py
+cp /home/oof/new_pdf_mutator/pdfium/*.py .
 cp /home/oof/new_pdf_mutator/resources.pkl .
+export ASAN_OPTIONS=alloc_dealloc_mismatch=0:allocator_may_return_null=1:halt_on_error=0
+# pdf_corpus
+# ASAN_OPTIONS=alloc_dealloc_mismatch=0:allocator_may_return_null=1:halt_on_error=1:abort_on_error=1 SLOT_INDEX=1 LIBFUZZER_PYTHON_MODULE=daemon PYTHONPATH=. ./pdfium_fuzzer -fork=1 -ignore_crashes=1 -jobs=1 -dict=pdfium_fuzzer.dict -timeout=10 -rss_limit_mb=0 ./smallcorpus/ # ./pdf_corpus/
 
-LIBFUZZER_PYTHON_MODULE=mutator PYTHONPATH=. ./pdfium_fuzzer -dict=pdfium_fuzzer.dict -timeout=3 ./pdf_corpus/
 
+ASAN_OPTIONS=alloc_dealloc_mismatch=0:allocator_may_return_null=1:halt_on_error=1:abort_on_error=1 SLOT_INDEX=1 LIBFUZZER_PYTHON_MODULE=daemon PYTHONPATH=. ./pdfium_fuzzer -fork=1 -ignore_crashes=1 -jobs=16 -dict=pdfium_fuzzer.dict -timeout=10 -rss_limit_mb=0 ./smallcorpus/
+
+
+# ASAN_OPTIONS=alloc_dealloc_mismatch=0:allocator_may_return_null=1:halt_on_error=1:abort_on_error=1 LIBFUZZER_PYTHON_MODULE=daemon PYTHONPATH=. ./pdfium_fuzzer -dict=pdfium_fuzzer.dict -timeout=10 -rss_limit_mb=0 ./smallcorpus/ # ./pdf_corpus/
 
